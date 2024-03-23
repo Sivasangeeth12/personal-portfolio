bda8e00a162cc7d663ddb29d3546b5a6f74fc4df
Sivasangeeth <sivasangeeth04@gmail.com>
2024-03-23 14:11:11 +0530

    made changes in colors

--- src/header/Button.jsx
+++ src/header/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = () => {
   return (
     <div className='button'>
-        <a href="https://drive.google.com/file/d/12tn-ujEwA6bZ8QT_kZfi2qrvf9-DDcEv/view?usp=sharing" target='_blank' className='btn'>Download CV</a>
+        <a href="https://drive.google.com/file/d/1AsCh-tNyOCszQc2Ub2e0VYh-bw0Hsvxj/view?usp=sharing" target='_blank' className='btn'>Download CV</a>
         {/* <a href="#contact" className='btn btn-primary'>Let's talk</a> */}
         
     </div>
